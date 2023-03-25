4941920267e6f06a16f1517cd3f0081f7bfeead1
koyamal <tanakamaruko1121n@gmail.com>
2023-03-25 09:03:35 +0900

    Add event handler of add button

--- 28/js/main.js
+++ 28/js/main.js
@@ -1,6 +1,11 @@
 const app = Vue.createApp({
     data: () => ({
-    })
+    }),
+    methods: {
+        addItem: function(event){
+            console.log('Button Clicked!');
+        }
+    }
 })
 
 app.mount('#app')
