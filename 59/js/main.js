5fdd13a7ca6d751fbf9dfc2b26e0ac2d9d0b1ce1
koyamal <tanakamaruko1121n@gmail.com>
2023-03-29 08:47:45 +0900

    Joint Name by computed

--- 59/js/main.js
+++ 59/js/main.js
@@ -1,17 +1,28 @@
+// const app = Vue.createApp({
+//     data: () => ({
+//         firstName: '',
+//         lastName: '',
+//         fullName: '',
+//     }),
+//     watch: {
+//         firstName: function(value){
+//             this.fullName = value + ' ' + this.lastName;
+//         },
+//         lastName: function(value){
+//             this.fullName = this.firstName + ' ' + value;
+//         }
+//     }
+// })
+
 const app = Vue.createApp({
     data: () => ({
         firstName: '',
         lastName: '',
-        fullName: '',
     }),
-    watch: {
-        firstName: function(value){
-            this.fullName = value + ' ' + this.lastName;
-        },
-        lastName: function(value){
-            this.fullName = this.firstName + ' ' + value;
+    computed: {
+        fullName: function(){
+            return this.firstName + ' ' + this.lastName;
         }
     }
 })
-
 app.mount('#app');
