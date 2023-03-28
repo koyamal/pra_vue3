8fabf378908a443dce66bdf96703908f1f8f858c
koyamal <tanakamaruko1121n@gmail.com>
2023-03-29 08:48:10 +0900

    Check reload timing of computed/methods

--- 55/js/main.js
+++ 55/js/main.js
@@ -1,10 +1,12 @@
 const app = Vue.createApp({
     data: () => ({
-
+        ranDomValue: 0,
+        hello: ""
     }),
     computed: {
         computedNumber: function(){
             console.log('Computed is called');
+            this.ranDomValue = this.hello
             return Math.random();
         }
     },
