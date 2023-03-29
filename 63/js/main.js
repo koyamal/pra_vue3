d6146fa061aae1338e8c932db1d1866abdccb7ee
koyamal <tanakamaruko1121n@gmail.com>
2023-03-30 07:38:30 +0900

    Use axios

--- 63/js/main.js
+++ 63/js/main.js
@@ -1,17 +1,31 @@
 const app = Vue.createApp({
     data: () => ({
         itmes: null,
-        keyword: '',
+        keyword: 'node.js',
         message: '',
     }),
     watch: {
 
     },
     mounted: function(){
-
+        this.getAnswer();
     },
     methods:{
+        getAnswer: function(){
+            if(this.keyword === ''){
+                console.log('Input Kyeword');
+                this.items = null;
+                return
+            }
 
+            this.message = 'Loading...';
+            const vm = this;
+            const params = {page: 1, per_page: 20, query: this.keyword}
+            axios.get('https://qiita.com/api/v2/items', {params})
+                .then(function(response){
+                    console.log(response);
+                })
+        }
     }
 })
 
