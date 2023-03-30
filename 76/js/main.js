4efbd33fed2ce89e64ba1b41a64fcb08d6471694
koyamal <tanakamaruko1121n@gmail.com>
2023-03-31 08:13:01 +0900

    Use ternary operator

---
+++ 76/js/main.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data: () => ({
+        largeClass:{
+            'large': true,
+            'bg-gray': true
+        },
+        dangerClass:{
+            'text-danger': true
+        },
+        isLarge: true
+    })
+})
+
+app.mount('#app');
