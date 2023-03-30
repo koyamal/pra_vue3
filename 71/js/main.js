ce2524420dfe0da936b478d1ff8a23086b85deb7
koyamal <tanakamaruko1121n@gmail.com>
2023-03-30 20:38:07 +0900

    Add change button

--- 71/js/main.js
+++ 71/js/main.js
@@ -1,7 +1,12 @@
 const app = Vue.createApp({
     data: () => ({
         isLarge: false
-    })
+    }),
+    methods:{
+        onClickChange: function(){
+            this.isLarge = !this.isLarge
+        }
+    }
 })
 
 app.mount('#app');
