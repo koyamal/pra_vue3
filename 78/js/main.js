4e2a522951eb1e87e34ed2cc19ac934176224520
koyamal <tanakamaruko1121n@gmail.com>
2023-04-01 18:35:21 +0900

    Add Change Button

--- 78/js/main.js
+++ 78/js/main.js
@@ -4,8 +4,21 @@ const app = Vue.createApp({
             color: 'blue',
             fontSize: '48px',
             fontWeight: 'bold'
+        },
+        text: 'Vue.js.',
+        flag: true
+    }),
+    methods:{
+        onClick: function(){
+            if(this.flag){
+                this.text = 'World.';
+                this.flag = false;
+            }else{
+                this.text = 'Vue.js.'
+                this.flag = true;
+            }
         }
-    })
+    }
 })
 
 app.mount('#app');
