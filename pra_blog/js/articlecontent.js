908d8a061d5ec8a87a3c717f2e9c0e7cea59d16f
koyamal <tanakamaruko1121n@gmail.com>
2023-04-03 19:05:51 +0900

    Add button changing display content or not

--- pra_blog/js/articlecontent.js
+++ pra_blog/js/articlecontent.js
@@ -2,12 +2,18 @@ export default {
     data: () => ({
         flag: false
     }),
+    methods:{
+        onClick: function(){
+            this.flag = !this.flag
+        }
+    },
     props: ['id', 'title','content'],
     template: `
     <div class="articleClass">
     <p>Id: {{ id }}</p>
     <p>Title: {{ title }}</p>
     <p v-if="flag">Content: {{ content }}</p>
+    <button v-on:click="onClick">{{!flag? "Show": "x"}}</button>
     </div>
     `
 }
