e796a0f8e240441a7d0fc7c98ebdaa7a1fe08a31
koyamal <tanakamaruko1121n@gmail.com>
2023-04-04 20:06:05 +0900

    Add articles

--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -1,8 +1,32 @@
 <script>
+let id = 1
 export default {
   data() {
     return {
-      greeting: 'Hello World!'
+      greeting: 'Hello World!',
+      articles: [
+        {
+          id: id++,
+          title: 'First article',
+          content: 'This is first article',
+          author: 'John',
+          upDateTime: '2023-02-04 19:02'
+        },
+        {
+          id: id++,
+          title: 'Second article',
+          content: 'This is second article',
+          author: 'Tom',
+          upDateTime: '2023-02-05 21:31'
+        },
+        {
+          id: id++,
+          title: 'Third article',
+          content: 'This is third article',
+          author: 'John',
+          upDateTime: '2023-02-06 08:22'
+        }
+      ]
     }
   },
   props: ['msg']
@@ -13,6 +37,11 @@ export default {
   <div>
     <h2>{{ greeting }}</h2>
     <p>{{ msg }}</p>
+    <div v-for="article in articles" :key="article.id">
+      <h2>{{ article.title }}</h2>
+      <p>{{ article.content }}</p>
+      <p>by {{ article.author }}</p>
+    </div>
   </div>
 </template>
 
