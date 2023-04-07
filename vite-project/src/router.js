f5685a748a8a65955f66fb40625a0271bb13e0f8
koyamal <tanakamaruko1121n@gmail.com>
2023-04-07 17:06:20 +0900

    Display AddArticle

--- vite-project/src/router.js
+++ vite-project/src/router.js
@@ -2,9 +2,10 @@ import { createRouter, createWebHashHistory } from "vue-router"
 const Home = { template: '<div>Homes</div>' }
 const About = { template: '<div>About</div>' }
 import HelloWorld from './components/HelloWorld.vue'
+import AddArticle from './components/AddArticle.vue'
 const routes = [
   { path: '/', component: HelloWorld },
-  { path: '/add', component: HelloWorld },
+  { path: '/add', component: AddArticle },
 ]
 const router = createRouter({
   history: createWebHashHistory(),
