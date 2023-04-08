d26e8e779144b6f1b6866b7adc8b722c2b85fc77
koyamal <tanakamaruko1121n@gmail.com>
2023-04-08 20:53:32 +0900

    Add moduleB

--- exportImport/js/main.js
+++ exportImport/js/main.js
@@ -1,8 +1,11 @@
 import anonymous from './modules/moduleA.js'
 import { aa, aaa } from './modules/moduleA.js'
+import { funcB } from './modules/moduleB.js';
 
 console.log(anonymous());
 
 console.log(aa);
 
 aaa();
+
+funcB(100)
---
+++ exportImport/js/modules/moduleB.js
@@ -0,0 +1,5 @@
+const funcB = function(val){
+    console.log(val * val)
+}
+
+export { funcB }
