e1d824797e588b303c93d45549a085cd234cf667
koyamal <tanakamaruko1121n@gmail.com>
2023-05-01 17:03:02 +0900

    Use v-on:click.shift

--- Vue3FrontDevTextBook/chap04/directive/src/App.vue
+++ Vue3FrontDevTextBook/chap04/directive/src/App.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-const msg = ref("No Send");
-const onFormSubmit = (): void => {
-  msg.value = "Send!!";
-}
+const msg = ref("Not yet");
+const onEnterKey = (): void => {
+  msg.value = 'Enter Key is pushed.'
+};
+const onRightButtonClick = (): void => {
+  msg.value = 'Right Button is pushed.'
+};
+const onShiftClick = (): void => {
+  msg.value = 'Shift is pushed.'
+};
 </script>
 
 <template>
-  <form action="#" v-on:submit.prevent="onFormSubmit">
-    <input type="text" required>
-    <button type="submit">Go.</button>
-  </form>
   <p>{{ msg }}</p>
+  <input type="text" v-on:keydown.enter="onEnterKey"><br>
+  <button v-on:click.right="onRightButtonClick">Right</button>
+  <button v-on:click.shift="onShiftClick">Shift</button>
 </template>
