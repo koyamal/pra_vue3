b0f94f8d3cbfe9d21799c55c433c9886f991e9a2
koyamal <tanakamaruko1121n@gmail.com>
2023-05-01 16:29:17 +0900

    Use v-on:mousemove

--- Vue3FrontDevTextBook/chap04/directive/src/App.vue
+++ Vue3FrontDevTextBook/chap04/directive/src/App.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-const randValue = ref("Not yet");
-const onButtonClick = (): void => {
-  const rand = Math.round(Math.random() * 10);
-  randValue.value = String(rand);
+const mousePointerX = ref(0);
+const mousePointerY = ref(0);
+const onImgMousemove = (event: MouseEvent): void => {
+  mousePointerX.value = event.offsetX;
+  mousePointerY.value = event.offsetY;
 }
 </script>
 
 <template>
   <section>
-    <button v-on:click="onButtonClick">Click!</button>
-    <p>Result: {{ randValue }}</p>
+    <img src="./assets/logo.svg" alt="VueLogo" width="200" v-on:mousemove="onImgMousemove" />
+    <p>Position of Pointer: X={{ mousePointerX }}, Y={{ mousePointerY }}</p>
   </section>
 </template>
