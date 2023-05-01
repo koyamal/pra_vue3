a170dd25105a0c1abca98542f102f88f338f4cd3
koyamal <tanakamaruko1121n@gmail.com>
2023-05-01 14:23:38 +0900

    Use Dynamic Arguments

--- Vue3FrontDevTextBook/chap04/directive/src/App.vue
+++ Vue3FrontDevTextBook/chap04/directive/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-const isSendButtonDisabled = ref(false);
+// const widthOrHeight = ref("height");
+const widthOrHeight = ref("width");
+const widthOrHeightValue = ref(1000);
 </script>
 
 <template>
-  <p><button type="button" v-bind:disabled="isSendButtonDisabled">Send</button></p>
+  <p><img alt="VueLog" src="./assets/logo.svg" v-bind:[widthOrHeight]="widthOrHeightValue"></p>
 </template>
