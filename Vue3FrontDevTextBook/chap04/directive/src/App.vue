4968434b50e2a673382f63022e6e5caa56f13897
koyamal <tanakamaruko1121n@gmail.com>
2023-05-01 16:34:28 +0900

    Use prop in v-on handler

--- Vue3FrontDevTextBook/chap04/directive/src/App.vue
+++ Vue3FrontDevTextBook/chap04/directive/src/App.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-const mousePointerX = ref(0);
-const mousePointerY = ref(0);
-const onImgMousemove = (event: MouseEvent): void => {
-  mousePointerX.value = event.offsetX;
-  mousePointerY.value = event.offsetY;
+const pBgColor = ref("white");
+const onPClick = (bgColor: string): void => {
+  pBgColor.value = bgColor;
 }
 </script>
 
 <template>
-  <section>
-    <img src="./assets/logo.svg" alt="VueLogo" width="200" v-on:mousemove="onImgMousemove" />
-    <p>Position of Pointer: X={{ mousePointerX }}, Y={{ mousePointerY }}</p>
-  </section>
+  <p v-on:click="onPClick('red')" v-bind:style="{backgroundColor: pBgColor}">
+    click
+  </p>
 </template>
