b38009d9a8d9e35c76686be35cbf047d7d55b14f
koyamal <tanakamaruko1121n@gmail.com>
2023-05-01 14:19:19 +0900

    Use v-bind for boolean prop

--- Vue3FrontDevTextBook/chap04/directive/src/App.vue
+++ Vue3FrontDevTextBook/chap04/directive/src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-const url = ref("https://vuejs.org/");
+const isSendButtonDisabled = ref(false);
 </script>
 
 <template>
-  <p><a v-bind:href="url" target="_blank">Vue.js</a></p>
-  <p><a :href="url" target="_blank">Vue.js(abbreviation)</a></p>
-  <p><a :href="url + 'guide/introduction.html'" target="_blank">Vue.js(guide)</a></p>
+  <p><button type="button" v-bind:disabled="isSendButtonDisabled">Send</button></p>
 </template>
