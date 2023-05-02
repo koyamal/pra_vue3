bf838efa09ff115bb60ae182e000c27c1e941e08
koyamal <tanakamaruko1121n@gmail.com>
2023-05-02 17:56:54 +0900

    Use v-model.trim

--- Vue3FrontDevTextBook/chap05/dataVinding/src/App.vue
+++ Vue3FrontDevTextBook/chap05/dataVinding/src/App.vue
@@ -1,42 +1,13 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-const inputTextarea = ref("Input Text Area \n 2Lines");
-const memberType = ref(1);
-const memberTypeSelect = ref(1);
-const isAgreed = ref(false);
-const isAgreed01 = ref(0);
-const selectedOS = ref([]);
-const selectedOSSelect = ref([]);
+const trimedText = ref("");
 </script>
 
 <template>
-  <textarea v-model="inputTextarea"></textarea>
-  <br>
   <section>
-    <label><input type="radio" name="memberType" value="1" v-model="memberType">Nomal</label>
-    <label><input type="radio" name="memberType" value="2" v-model="memberType">Special</label>
-    <label><input type="radio" name="memberType" value="3" v-model="memberType">VIP</label>
-    <br>
-    <p>Selected: {{ memberType }}</p>
-  </section>
-  <br>
-  <section>
-    <select v-model="memberTypeSelect">
-      <option value="1">Nomal</option>
-      <option value="2">Special</option>
-      <option value="3">VIP</option>
-    </select>
-    <p>Selected: {{ memberTypeSelect }}</p>
-  </section>
-  <br>
-  <section>
-    <label><input type="checkbox" v-model="isAgreed">Agree</label>
-    <p>Result: {{ isAgreed }}</p>
-  </section>
-  <section>
-    <label><input type="checkbox" v-model="isAgreed01" true-value="1" false-value="0">Agree</label>
-    <p>Result: {{ isAgreed01 }}</p>
+    <input type="text" v-model.trim="trimedText">
+    <p>Text: {{ trimedText }}</p>
   </section>
 </template>
 
