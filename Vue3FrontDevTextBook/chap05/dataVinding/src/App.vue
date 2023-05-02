f60ab86d13912bb18bc393e3f75ddda77682ade3
koyamal <tanakamaruko1121n@gmail.com>
2023-05-02 18:10:55 +0900

    Use v-cloak

--- Vue3FrontDevTextBook/chap05/dataVinding/src/App.vue
+++ Vue3FrontDevTextBook/chap05/dataVinding/src/App.vue
@@ -1,10 +1,15 @@
+<script setup lang="ts">
+import {ref} from "vue";
 
+const hello = ref("hello");
+</script>
 
 <template>
-  <section v-pre>
-    <p v-on:click="showHello">{{ hello }}</p>
-  </section>
+  <p v-cloak>{{ hello }}</p>
 </template>
 
 <style scoped>
+[v-cloak] {
+  display: none;
+}
 </style>
