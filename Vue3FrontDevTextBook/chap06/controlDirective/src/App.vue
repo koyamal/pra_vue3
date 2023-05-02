b7c3362071829f73097ef6e8ed08463531806f93
koyamal <tanakamaruko1121n@gmail.com>
2023-05-03 08:46:15 +0900

    Use v-show

--- Vue3FrontDevTextBook/chap06/controlDirective/src/App.vue
+++ Vue3FrontDevTextBook/chap06/controlDirective/src/App.vue
@@ -1,21 +1,27 @@
 <script setup lang="ts">
 import {computed} from "vue";
 
-const randomNumber = computed(
-  ():number => {
-    return Math.round(Math.random() * 100);
+const showOrNot = computed(
+  (): boolean => {
+    let showOrNot = false;
+    const rand = Math.round(Math.random() * 100);
+    if(rand >= 50) {
+      showOrNot = true;
+    }
+    return showOrNot;
   }
-)
+);
 </script>
 
 <template>
-  <p>
-    Score: {{ randomNumber }}
-    <template v-if="randomNumber >= 80">
-      Good.
-      <span style="color: red">Nice!</span>
-    </template>
-  </p>
+  <section>
+    v-if:
+    <p v-if="showOrNot">Trueeee</p>
+  </section>
+  <section>
+    v-show:
+    <p v-show="showOrNot">Trueeee</p>
+  </section>
 </template>
 
 <style scoped>
