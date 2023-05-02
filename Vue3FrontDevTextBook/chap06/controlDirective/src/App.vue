5b1f7d3f81a4fec51220412e033610b3d411686d
koyamal <tanakamaruko1121n@gmail.com>
2023-05-03 08:57:32 +0900

    Use v-for

--- Vue3FrontDevTextBook/chap06/controlDirective/src/App.vue
+++ Vue3FrontDevTextBook/chap06/controlDirective/src/App.vue
@@ -1,27 +1,25 @@
 <script setup lang="ts">
-import {computed} from "vue";
+import {ref} from "vue";
 
-const showOrNot = computed(
-  (): boolean => {
-    let showOrNot = false;
-    const rand = Math.round(Math.random() * 100);
-    if(rand >= 50) {
-      showOrNot = true;
-    }
-    return showOrNot;
-  }
-);
+const cocktailListInit: string[] = ["WhiteLady", "BlueHawaii", "NewYork"];
+const cocktailList = ref(cocktailListInit);
 </script>
 
 <template>
-  <section>
-    v-if:
-    <p v-if="showOrNot">Trueeee</p>
-  </section>
-  <section>
-    v-show:
-    <p v-show="showOrNot">Trueeee</p>
-  </section>
+  <ul>
+    <li
+      v-for="cocktailName in cocktailList"
+      v-bind:key="cocktailName">
+      {{ cocktailName }}
+    </li>
+  </ul>
+  <ul>
+    <li
+      v-for="(cocketailName, index) in cocktailList"
+      v-bind:key="cocketailName">
+      {{ cocketailName }}(No.{{ index }})
+    </li>
+  </ul>
 </template>
 
 <style scoped>
