b6339c9c236667de4584d7ef8cbf5db8715b0035
koyamal <tanakamaruko1121n@gmail.com>
2023-05-02 18:17:02 +0900

    Use v-if

--- Vue3FrontDevTextBook/chap06/controlDirective/src/App.vue
+++ Vue3FrontDevTextBook/chap06/controlDirective/src/App.vue
@@ -1,47 +1,25 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {computed, ref} from "vue";
+
+const number = ref(80);
+const showOrNot = computed(
+  (): boolean => {
+    let showOrNot = false;
+    const rand = Math.round(Math.random() * 100);
+    if(rand >= 50) {
+      showOrNot = true;
+    }
+    return showOrNot;
+  }
+);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p v-if="number >= 50">True. 1</p>
+  <p v-if="Math.round(Math.random() * 100) >= 50">True. 2</p>
+  <p v-if="showOrNot">True. 3</p>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
