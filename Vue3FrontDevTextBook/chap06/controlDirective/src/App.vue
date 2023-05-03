8eddc078cfabef53d04d3bfda0f36dd9b2c09575
koyamal <tanakamaruko1121n@gmail.com>
2023-05-03 09:07:33 +0900

    Use v-for

--- Vue3FrontDevTextBook/chap06/controlDirective/src/App.vue
+++ Vue3FrontDevTextBook/chap06/controlDirective/src/App.vue
@@ -1,23 +1,29 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-const cocktailListInit: string[] = ["WhiteLady", "BlueHawaii", "NewYork"];
+const cocktailListInit: {[key: number]: string} = {
+  2345: "WhiteLady",
+  4412: "BlueHawaii",
+  6792: "NewYork",
+};
 const cocktailList = ref(cocktailListInit);
 </script>
 
 <template>
   <ul>
     <li
-      v-for="cocktailName in cocktailList"
-      v-bind:key="cocktailName">
-      {{ cocktailName }}
+      v-for="(cocktailName, id) in cocktailList"
+      v-bine:key="'cocktailList' + id"
+    >
+      ID-{{ id }}: {{ cocktailName }}
     </li>
   </ul>
   <ul>
     <li
-      v-for="(cocketailName, index) in cocktailList"
-      v-bind:key="cocketailName">
-      {{ cocketailName }}(No.{{ index }})
+      v-for="(cocktailName, id, index) in cocktailList"
+      v-bine:key="'cocktailListwith' + id"
+    >
+      {{ index + 1 }}. ID-{{ id }}: {{ cocktailName }}
     </li>
   </ul>
 </template>
