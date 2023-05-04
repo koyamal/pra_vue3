e024909f1c5a48563d48435235402df7a7f03bc6
koyamal <tanakamaruko1121n@gmail.com>
2023-05-04 14:52:17 +0900

    Create OneSection Component and Use

--- Vue3FrontDevTextBook/chap08/linkComponents/src/App.vue
+++ Vue3FrontDevTextBook/chap08/linkComponents/src/App.vue
@@ -1,8 +1,24 @@
 <script setup lang="ts">
+import OneSection from './components/OneSection.vue';
 </script>
 
 <template>
+  <h1>Basic Component</h1>
+  <section>
+    <h2>One Component</h2>
+    <OneSection />
+  </section>
+  <section>
+    <h2>Multiple Components</h2>
+    <OneSection />
+    <OneSection />
+    <OneSection />
+  </section>
 </template>
 
-<style scoped>
+<style>
+section {
+  border: blue 1px solid;
+  margin: 10px
+}
 </style>
---
+++ Vue3FrontDevTextBook/chap08/linkComponents/src/components/OneSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="box">
+    <h4>The Component</h4>
+    <p>
+      Component is ...
+    </p>
+  </section>
+</template>
+
+<style>
+.box{
+  border: green 1px dashed;
+  margin: 10px;
+}
+</style>
