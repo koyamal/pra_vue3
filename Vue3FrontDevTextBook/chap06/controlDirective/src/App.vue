252dcc6f8091ba651ac0122e36c1261d7aefe9a9
koyamal <tanakamaruko1121n@gmail.com>
2023-05-04 10:03:37 +0900

    Use v-for in map

--- Vue3FrontDevTextBook/chap06/controlDirective/src/App.vue
+++ Vue3FrontDevTextBook/chap06/controlDirective/src/App.vue
@@ -1,30 +1,20 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-const cocktailListInit: {[key: number]: string} = {
-  2345: "WhiteLady",
-  4412: "BlueHawaii",
-  6792: "NewYork",
-};
+const cocktailListInit = new Map<number, string>();
+cocktailListInit.set(2345, "WhiteLady");
+cocktailListInit.set(4412, "BlueHawaii");
+cocktailListInit.set(6792, "NewYork");
 const cocktailList = ref(cocktailListInit);
-console.log(cocktailListInit);
 </script>
 
 <template>
   <ul>
     <li
-      v-for="(cocktailName, id) in cocktailList"
-      v-bine:key="'cocktailList' + id"
+      v-for="[id, cocktailName] in cocktailList"
+      v-bind:key="id"
     >
-      ID-{{ id }}: {{ cocktailName }}
-    </li>
-  </ul>
-  <ul>
-    <li
-      v-for="(cocktailName, id, index) in cocktailList"
-      v-bine:key="'cocktailListwith' + id"
-    >
-      {{ index + 1 }}. ID-{{ id }}: {{ cocktailName }}
+      ID:{{ id }}. {{ cocktailName }}
     </li>
   </ul>
 </template>
