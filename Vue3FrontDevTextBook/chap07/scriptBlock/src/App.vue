d621caa25172e41f064cff156290a068e1b96293
koyamal <tanakamaruko1121n@gmail.com>
2023-05-04 14:24:45 +0900

    Use Options API

--- Vue3FrontDevTextBook/chap07/scriptBlock/src/App.vue
+++ Vue3FrontDevTextBook/chap07/scriptBlock/src/App.vue
@@ -1,50 +1,31 @@
 <script lang="ts">
-import { defineComponent, ref, computed } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: "App",
-  setup(){
-    const cocktailDataListInit = new Map<number, Cocktail>();
-    cocktailDataListInit.set(1, {id: 1, name: "WhiteLady", price: 1200});
-    cocktailDataListInit.set(2, {id: 2, name: "BlueHawaii", price: 1500});
-    cocktailDataListInit.set(3, {id: 3, name: "NewYork", price: 1100});
-    cocktailDataListInit.set(4, {id: 4, name: "Matani", price: 1500});
-    const cocktailNo = ref(1);
-
-    const priceMsg = computed(
-      (): string => {
-        const cocktail = cocktailDataListInit.get(cocktailNo.value);
-        let msg = `No Cocktail No.${cocktailNo.value}`
-        if(cocktail != undefined){
-          msg = `No.${cocktailNo.value} is ${cocktail.name}. Price: ${cocktail.price}`;
-        }
-        return msg;
-      }
-    );
-
-    setInterval(
-      (): void => {
-        cocktailNo.value = Math.round(Math.random() * 3) + 1;
-      },
-      1000
-    );
+  data() {
     return {
-      cocktailNo,
-      priceMsg,
-    };
+      height: Math.round(Math.random() * 10),
+      width: Math.round(Math.random() * 10),
+    }
+  },
+  computed: {
+    area(): number{
+      return this.height * this.width;
+    }
+  },
+  methods: {
+    change(): void {
+      this.height = Math.round(Math.random() * 10);
+      this.width = Math.round(Math.random() * 10);
+    }
   }
 });
-
-interface Cocktail {
-  id: number;
-  name: string;
-  price: number;
-};
 </script>
 
 <template>
-  <p>Cocktail No. {{ cocktailNo }}</p>
-  <p>{{ priceMsg }}</p>
+  <p>Height: {{ height }}, Width: {{ width }} ==> Area: {{ area }}</p>
+  <button v-on:click="change">Change</button>
 </template>
 
 <style scoped>
