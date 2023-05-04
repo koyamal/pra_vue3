0712d67aac8c94de975794c02767f40065c2aa9d
koyamal <tanakamaruko1121n@gmail.com>
2023-05-04 10:41:59 +0900

    Use computed/filter

--- Vue3FrontDevTextBook/chap06/controlDirective/src/App.vue
+++ Vue3FrontDevTextBook/chap06/controlDirective/src/App.vue
@@ -1,30 +1,54 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {ref, computed} from "vue";
 
-const cocktailDataListInit = new Map<number, Cocktail>();
-cocktailDataListInit.set(2345, {id: 2345, name: "WhiteLady", price: 1200});
-cocktailDataListInit.set(4412, {id: 4412, name: "BlueHawaii", price: 1200});
-cocktailDataListInit.set(6792, {id: 6792, name: "NewYork", price: 1200});
-cocktailDataListInit.set(8429, {id: 8429, name: "Matani", price: 1200});
-
+const cocktailDataListInit: Cocktail[] = [
+  {id: 2345, name: "WhiteLady", price: 1200},
+  {id: 4412, name: "BlueHawaii", price: 1500},
+  {id: 6792, name: "NewYork", price: 1200},
+  {id: 8429, name: "Matani", price: 1500},
+];
 const cocktailDataList = ref(cocktailDataListInit);
-
 interface Cocktail {
   id: number;
   name: string;
   price: number;
-}
+};
+const cocktail1500 = computed(
+  (): Cocktail[] => {
+    const newList = cocktailDataList.value.filter(
+      (cocktailItem: Cocktail): boolean => {
+        return cocktailItem.price == 1500;
+      }
+    );
+    return newList
+  }
+);
+
 </script>
 
 <template>
-  <ul>
-    <li
-      v-for="[id, cocktailItem] in cocktailDataList"
-      v-bind:key="id"
-    >
-      {{ cocktailItem.name }}: ¥{{ cocktailItem.price }}
-    </li>
-  </ul>
+  <section>
+    All List
+    <ul>
+      <li
+        v-for="cocktailItem in cocktailDataList"
+        v-bind:key="cocktailItem.id"
+      >
+        {{ cocktailItem.name }}: ¥{{ cocktailItem.price }}
+      </li>
+    </ul>
+  </section>
+  <section>
+    ¥1500 Menu:
+    <ul>
+      <li
+        v-for="cocktailItem in cocktail1500"
+        v-bind:key="'1500' + cocktailItem.id"
+      >
+        {{ cocktailItem.name }}: ¥{{ cocktailItem.price }}
+      </li>
+    </ul>
+  </section>
 </template>
 
 <style scoped>
