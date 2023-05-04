05d97af8db442b9f53ff3b6cb2aaae04bd1f0244
koyamal <tanakamaruko1121n@gmail.com>
2023-05-04 10:51:34 +0900

    Edit(Add, Delete, Change) list

--- Vue3FrontDevTextBook/chap06/controlDirective/src/App.vue
+++ Vue3FrontDevTextBook/chap06/controlDirective/src/App.vue
@@ -1,54 +1,31 @@
 <script setup lang="ts">
 import {ref, computed} from "vue";
 
-const cocktailDataListInit: Cocktail[] = [
-  {id: 2345, name: "WhiteLady", price: 1200},
-  {id: 4412, name: "BlueHawaii", price: 1500},
-  {id: 6792, name: "NewYork", price: 1200},
-  {id: 8429, name: "Matani", price: 1500},
-];
-const cocktailDataList = ref(cocktailDataListInit);
-interface Cocktail {
-  id: number;
-  name: string;
-  price: number;
+const cocktailListInit: string[] = ["WhiteLady", "BlueHawaii", "NewYork"];
+const cocktailList = ref(cocktailListInit);
+const changeCocktailList = (): void => {
+  cocktailList.value = ["Balalaika", "XYZ", "Manhattan"];
 };
-const cocktail1500 = computed(
-  (): Cocktail[] => {
-    const newList = cocktailDataList.value.filter(
-      (cocktailItem: Cocktail): boolean => {
-        return cocktailItem.price == 1500;
-      }
-    );
-    return newList
-  }
-);
-
+const addCocktailList = (): void => {
+  cocktailList.value.push("BlueMoon");
+};
+const deleteFromCocktailList = (): void => {
+  cocktailList.value.pop();
+}
 </script>
 
 <template>
-  <section>
-    All List
-    <ul>
-      <li
-        v-for="cocktailItem in cocktailDataList"
-        v-bind:key="cocktailItem.id"
-      >
-        {{ cocktailItem.name }}: ¥{{ cocktailItem.price }}
-      </li>
-    </ul>
-  </section>
-  <section>
-    ¥1500 Menu:
-    <ul>
-      <li
-        v-for="cocktailItem in cocktail1500"
-        v-bind:key="'1500' + cocktailItem.id"
-      >
-        {{ cocktailItem.name }}: ¥{{ cocktailItem.price }}
-      </li>
-    </ul>
-  </section>
+  <ul>
+    <li
+      v-for="(cocktailName, index) in cocktailList"
+      v-bind:key="cocktailName"
+    >
+      {{ cocktailName }}(No.{{ index }})
+    </li>
+  </ul>
+  <p><button v-on:click="changeCocktailList">Change</button></p>
+  <p><button v-on:click="addCocktailList">Add</button></p>
+  <p><button v-on:click="deleteFromCocktailList">Delete</button></p>
 </template>
 
 <style scoped>
