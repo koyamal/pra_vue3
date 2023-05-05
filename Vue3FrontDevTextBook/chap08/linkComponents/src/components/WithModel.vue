9e62f1ab2d567e2f73dcd80aa0dadc426a6248db
koyamal <tanakamaruko1121n@gmail.com>
2023-05-05 09:32:15 +0900

    Create WithModel

---
+++ Vue3FrontDevTextBook/chap08/linkComponents/src/components/WithModel.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const name = ref("No name");
+</script>
+
+<template>
+  <section>
+    <p>{{ name }}!</p>
+    <input type="text" v-model="name">
+  </section>
+</template>
+
+<style scoped>
+section{
+  border: orange 1px dashed;
+  margin: 10px;
+}
+</style>
