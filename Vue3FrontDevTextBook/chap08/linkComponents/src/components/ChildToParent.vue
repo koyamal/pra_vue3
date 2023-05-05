c0ed51655797fbe0426de4b1b97fd84ec0d9d6b1
koyamal <tanakamaruko1121n@gmail.com>
2023-05-05 17:57:06 +0900

    Create ChildToParent Component

---
+++ Vue3FrontDevTextBook/chap08/linkComponents/src/components/ChildToParent.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface Props {
+  rand: number;
+}
+
+interface Emits {
+  (event: "createNewRand"): void;
+}
+
+defineProps<Props>();
+const emit = defineEmits<Emits>();
+
+const onNewRandButtonClick = (): void => {
+  emit("createNewRand");
+}
+</script>
+
+<template>
+  <section class="box">
+    <p>Random Value in Child Component: {{ rand }}</p>
+    <button v-on:click="onNewRandButtonClick">Generate</button>
+  </section>
+</template>
+
+<style scoped>
+.box{
+  border: green 1px solid;
+  margin: 10px;
+}
+</style>
