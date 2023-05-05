b8fb28859985c82e5f25ca68db6c4df7bd1c02b6
koyamal <tanakamaruko1121n@gmail.com>
2023-05-05 09:38:44 +0900

    Use WithModel Component in App

--- Vue3FrontDevTextBook/chap08/linkComponents/src/App.vue
+++ Vue3FrontDevTextBook/chap08/linkComponents/src/App.vue
@@ -1,24 +1,19 @@
 <script setup lang="ts">
-import OneSection from './components/OneSection.vue';
+import WithModel from './components/WithModel.vue';
 </script>
 
 <template>
-  <h1>Basic Component</h1>
+  <h1>Component Independence</h1>
   <section>
-    <h2>One Component</h2>
-    <OneSection />
-  </section>
-  <section>
-    <h2>Multiple Components</h2>
-    <OneSection />
-    <OneSection />
-    <OneSection />
+    <h2>V-model Component</h2>
+      <WithModel />
+      <WithModel />
   </section>
 </template>
 
 <style>
 section {
   border: blue 1px solid;
-  margin: 10px
+  margin: 10px;
 }
 </style>
