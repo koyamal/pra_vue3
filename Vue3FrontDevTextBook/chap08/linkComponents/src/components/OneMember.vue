a2b095f4effc95b125a57dc8935d7f4c8f936f73
koyamal <tanakamaruko1121n@gmail.com>
2023-05-05 10:30:51 +0900

    Create OneMember Component

---
+++ Vue3FrontDevTextBook/chap08/linkComponents/src/components/OneMember.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import {ref, computed} from "vue";
+
+interface Props{
+  id: number;
+  name: string;
+  email: string;
+  points: number;
+  note?: string;
+}
+
+const props = defineProps<Props>();
+
+const localPoints = ref(props.points);
+const localNote = computed(
+  (): string => {
+    let localNote = props.note;
+    if(localNote == undefined){
+      localNote = "--";
+    }
+    return localNote;
+  }
+);
+const pointUp = (): void => {
+  localPoints.value++;
+}
+</script>
+
+<template>
+  <section class="box">
+    <h4>Info: {{ name }}</h4>
+    <dl>
+      <dt>ID</dt>
+      <dd>{{ id }}</dd>
+      <dt>E-mail</dt>
+      <dd>{{ email }}</dd>
+      <dt>Point</dt>
+      <dd>{{ localPoints }}</dd>
+      <dt>Other</dt>
+      <dd>{{ localNote }}</dd>
+    </dl>
+    <button v-on:click="pointUp">Point Up</button>
+  </section>
+</template>
+
+<style scoped>
+.box{
+  border: green 1px solid;
+  margin: 10px;
+}
+</style>
