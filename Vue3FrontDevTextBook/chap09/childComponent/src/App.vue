9b63fe2e53fe89439c0d2c63af0017e6e2b338db
koyamal <tanakamaruko1121n@gmail.com>
2023-05-06 15:11:23 +0900

    Use OneSection component in App

--- Vue3FrontDevTextBook/chap09/childComponent/src/App.vue
+++ Vue3FrontDevTextBook/chap09/childComponent/src/App.vue
@@ -1,47 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref} from "vue";
+import OneSection from "./components/OneSection.vue";
+
+const taro = ref("Taro");
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <section>
+    <h2>Use of Slot</h2>
+    <OneSection v-bind:name="taro">
+      <p>Cannot Reach.</p>
+    </OneSection>
+  </section>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
 </style>
