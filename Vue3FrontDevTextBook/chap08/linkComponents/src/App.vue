03a9172a63fc6000abefbfb5851f185fc68d6790
koyamal <tanakamaruko1121n@gmail.com>
2023-05-06 11:27:51 +0900

    Use provide/BaseSection in App

--- Vue3FrontDevTextBook/chap08/linkComponents/src/App.vue
+++ Vue3FrontDevTextBook/chap08/linkComponents/src/App.vue
@@ -1,40 +1,14 @@
 <script setup lang="ts">
-import {ref, computed} from "vue";
-import OneMember from "./components/OneMember.vue";
-const memberListInit = new Map<number, Member>();
-memberListInit.set(33456, {id: 33456, name: "Taro", email: "taro@exmaple.com", points: 35, note:"VIP"});
-memberListInit.set(47783, {id: 47783, name: "Jiro", email: "jiro@exmaple.com", points: 53});
-const memberList = ref(memberListInit);
-const totalPoints = computed(
-  (): number => {
-    let total = 0;
-    for(const member of memberList.value.values()){
-      total += member.points;
-    }
-    return total;
-  }
-);
-interface Member{
-  id: number;
-  name: string;
-  email: string;
-  points: number;
-  note?: string;
-};
+import {reactive, provide} from "vue";
+import BaseSection from "./components/BaseSection.vue";
+import type {Member} from "./interfaces";
+
+const memberList = new Map<number, Member>();
+memberList.set(33456, {id: 33456, name: "Taro", email: "taro@exmaple.com", points: 35, note:"VIP"});
+memberList.set(47783, {id: 47783, name: "Jiro", email: "jiro@exmaple.com", points: 53});
+provide("memberList", reactive(memberList));
 </script>
 
 <template>
-  <section>
-    <h1>Member's List</h1>
-    <p>Sum of Points: {{ totalPoints }}</p>
-    <OneMember
-      v-for="[id, member] in memberList"
-      v-bind:key="id"
-      v-bind:id="id"
-      v-bind:name="member.name"
-      v-bind:email="member.email"
-      v-model:points="member.points"
-      v-bind:note="member.note"
-    />
-  </section>
+  <BaseSection />
 </template>
