f2e86f595dd8bf32bca090fddef7e2cbfb02caed
koyamal <tanakamaruko1121n@gmail.com>
2023-05-06 11:28:21 +0900

    Use inject/OneMember in BaseSection

---
+++ Vue3FrontDevTextBook/chap08/linkComponents/src/components/BaseSection.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {computed, inject} from "vue";
+import OneMember from "./OneMember.vue";
+import type {Member} from "../interfaces";
+
+const memberList = inject("memberList") as Map<number, Member>;
+
+const totalPoints = computed(
+  (): number => {
+    let total = 0;
+    for(const member of memberList.values()){
+      total += member.points;
+    }
+    return total;
+  }
+)
+</script>
+
+<template>
+  <section>
+    <h1>Member's List</h1>
+    <p>Sum of Points: {{ totalPoints }}</p>
+    <OneMember
+      v-for="id in memberList.keys()"
+      v-bind:key="id"
+      v-bind:id="id"
+    />
+  </section>
+</template>
+
+<style scoped>
+section {
+  border: orange 1px dashed;
+  margin: 10px;
+}
+</style>
