01e4d8d95125992ed8469c0d9a163d5de45fa280
koyamal <tanakamaruko1121n@gmail.com>
2023-05-06 17:34:32 +0900

    Create Radio Component

---
+++ Vue3FrontDevTextBook/chap09/childComponent/src/components/Radio.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const memberType = ref(1);
+</script>
+
+<template>
+  <label>
+    <input type="radio" name="memberType" value="1" v-model="memberType">
+    Regular Member
+  </label>
+  <label>
+    <input type="radio" name="memberType" value="2" v-model="memberType">
+    Special Member
+  </label>
+  <label>
+    <input type="radio" name="memberType" value="3" v-model="memberType">
+    VIP Member
+  </label>
+  <br>
+  <p>Selected Value: {{ memberType }}</p>
+</template>
