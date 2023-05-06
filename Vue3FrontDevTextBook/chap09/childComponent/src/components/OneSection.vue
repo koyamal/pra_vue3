e62ce7e7d129b52f269359fea9b157e7e707f59a
koyamal <tanakamaruko1121n@gmail.com>
2023-05-06 15:07:12 +0900

    Create OneSection Component

---
+++ Vue3FrontDevTextBook/chap09/childComponent/src/components/OneSection.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+  name: string;
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <section class="box">
+    <h1>{{ name }}'s Condition</h1>
+    <slot />
+  </section>
+</template>
+
+<style>
+.box{
+  border: green 1px solid;
+}
+</style>
