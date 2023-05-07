05a3d72d4f9939b149227d6ccc64d50092c871ca
koyamal <tanakamaruko1121n@gmail.com>
2023-05-07 12:22:49 +0900

    Add routerLink to Top from MemberList

--- Vue3FrontDevTextBook/chap10/myvuerouter/src/views/member/MemberList.vue
+++ Vue3FrontDevTextBook/chap10/myvuerouter/src/views/member/MemberList.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+import {inject} from "vue";
+import {RouterLink} from "vue-router";
+import type {Member} from "@/interface";
+
+const memberList = inject("memberList") as Map<number, Member>;
+</script>
+
 <template>
   <h1>Member Management</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li>
+        <RouterLink v-bind:to="{name: 'AppTop'}">
+          TOP
+        </RouterLink>
+      </li>
+    </ul>
+  </nav>
   <section>
     <h2>MemberList</h2>
   </section>
