3208c9065171b2e5dcb28ce16c2cd2a48c61d949
koyamal <tanakamaruko1121n@gmail.com>
2023-05-07 12:38:57 +0900

    Add RouterLink to MemberDetail with params from MemberList

--- Vue3FrontDevTextBook/chap10/myvuerouter/src/views/member/MemberList.vue
+++ Vue3FrontDevTextBook/chap10/myvuerouter/src/views/member/MemberList.vue
@@ -15,9 +15,24 @@ const memberList = inject("memberList") as Map<number, Member>;
           TOP
         </RouterLink>
       </li>
+      <li>
+        MemberList
+      </li>
     </ul>
   </nav>
   <section>
     <h2>MemberList</h2>
+    <section>
+      <ul>
+        <li
+          v-for="[id, member] in memberList"
+          v-bind:key="id"
+        >
+          <RouterLink v-bind:to="{name: 'MemberDetail', params: {id: id}}">
+            ID-{{ id }}: {{ member.name }}
+          </RouterLink>
+        </li>
+      </ul>
+    </section>
   </section>
 </template>
