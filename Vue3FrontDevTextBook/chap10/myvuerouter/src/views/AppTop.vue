f4b26e80e436677ff1fc6c6de78cae7ead25b6ee
koyamal <tanakamaruko1121n@gmail.com>
2023-05-07 11:48:55 +0900

    Create Top Screen

---
+++ Vue3FrontDevTextBook/chap10/myvuerouter/src/views/AppTop.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>TOP</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li>TOP</li>
+    </ul>
+  </nav>
+  <section>
+    <p>
+      MemberList is here.
+    </p>
+  </section>
+</template>
