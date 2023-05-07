586429a40e164f8f12f8030dd6233248a39b22f5
koyamal <tanakamaruko1121n@gmail.com>
2023-05-07 12:38:15 +0900

    Create MemberDetail screen

---
+++ Vue3FrontDevTextBook/chap10/myvuerouter/src/views/member/MemberDetail.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Member Management</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li>
+        <RouterLink v-bind:to="{name: 'AppTop'}">
+          TOP
+        </RouterLink>
+      </li>
+      <li>
+        <RouterLink v-bind:to="{name: 'MemberList'}">
+          MemberList
+        </RouterLink>
+      </li>
+      <li>Member Detail</li>
+    </ul>
+  </nav>
+  <section>
+    <h2>Detail</h2>
+  </section>
+</template>
