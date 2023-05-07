84d85e5313ce92621a88f0d580101891f13ab692
koyamal <tanakamaruko1121n@gmail.com>
2023-05-07 11:48:11 +0900

    Use RouterView

--- Vue3FrontDevTextBook/chap10/myvuerouter/src/App.vue
+++ Vue3FrontDevTextBook/chap10/myvuerouter/src/App.vue
@@ -1,28 +1,45 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import Input from "./components/Input.vue";
-import Radio from "./components/Radio.vue";
-import Select from "./components/Select.vue";
+import {reactive, provide} from "vue";
+import {RouterView} from "vue-router";
+import type {Member} from "@/interface";
 
-const currentComp = ref(Input);
-const currentCompName = ref("Input");
-const compList = [Input, Radio, Select];
-const compNameList = ["Input", "Radio", "Select"];
-let currentCompIndex = 0;
-const switchComp = (): void => {
-  currentCompIndex++;
-  if(currentCompIndex >= 3){
-    currentCompIndex = 0;
-  }
-  currentComp.value = compList[currentCompIndex];
-  currentCompName.value = compNameList[currentCompIndex];
-}
+const memberList = new Map<number, Member>();
+memberList.set(33456, {id: 33456, name: "Taro", email: "taro@example.com", points: 35, note: "VIP"});
+memberList.set(47783, {id: 47783, name: "Jiro", email: "jiro@example.com", points: 45});
+provide("memberList", reactive(memberList));
 </script>
 
 <template>
-  <p>Component Name: {{ currentCompName }}</p>
-  <KeepAlive>
-    <component v-bind:is="currentComp" />
-  </KeepAlive>
-  <button v-on:click="switchComp">Switch</button>
+  <header>
+    <h1>Vue Router Sample</h1>
+  </header>
+  <main>
+    <RouterView />
+  </main>
 </template>
+
+<style>
+main {
+  border: blue 1px solid;
+  padding: 10px;
+}
+#breadcrumbs ul li {
+  display: inline;
+  list-style-type: none;
+}
+#breadcrumbs {
+  margin-left: 0px;
+}
+#breadcrumbs ul {
+  padding-left: 0px;
+}
+#breadcrumbs ul .current {
+  color: red;
+}
+#breadcrumbs ul li:before {
+  content: " > ";
+}
+#breadcrumbs ul li:first-child:before {
+  content: none;
+}
+</style>
