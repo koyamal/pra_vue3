81a3c94ecd39faa43fb8fe38e4c48f48ef4f56b6
koyamal <tanakamaruko1121n@gmail.com>
2023-05-08 11:30:25 +0900

    Migrate chap10

---
+++ Vue3FrontDevTextBook/chap11/myUseRouterPinia/src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<script setupt lang="ts">
+import {RouterLink} from "vue-router";
+</script>
+<template>
+  <h1>404 Not Found</h1>
+  <section>
+    <p>
+      Top Page is
+        <RouterLink v-bind:to="{name: 'AppTop'}">
+          here
+        </RouterLink>
+    </p>
+  </section>
+</template>
---
+++ Vue3FrontDevTextBook/chap11/myUseRouterPinia/src/views/member/MemberList.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {inject} from "vue";
+import {RouterLink, RouterView} from "vue-router";
+import type {Member} from "@/interface";
+
+const memberList = inject("memberList") as Map<number, Member>;
+</script>
+
+<template>
+  <h1>Member Management</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li>
+        <RouterLink v-bind:to="{name: 'AppTop'}">
+          TOP
+        </RouterLink>
+      </li>
+      <li>
+        MemberList
+      </li>
+    </ul>
+  </nav>
+  <section>
+    <h2>MemberList</h2>
+    <p>
+      Registration is
+        <RouterLink v-bind:to="{name: 'MemberAdd'}">
+          here
+        </RouterLink>
+    </p>
+    <section>
+      <ul>
+        <li
+          v-for="[id, member] in memberList"
+          v-bind:key="id"
+        >
+          <RouterLink v-bind:to="{name: 'MemberDetail', params: {id: id}}">
+            ID-{{ id }}: {{ member.name }}
+          </RouterLink>
+        </li>
+      </ul>
+    </section>
+    <RouterView />
+  </section>
+</template>
