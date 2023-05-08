cad646c613905d61384e24c7a9ccc69b33f61502
koyamal <tanakamaruko1121n@gmail.com>
2023-05-08 11:07:03 +0900

    Use computed

--- Vue3FrontDevTextBook/chap11/myUsePinia/src/App.vue
+++ Vue3FrontDevTextBook/chap11/myUsePinia/src/App.vue
@@ -3,19 +3,19 @@ import {computed} from "vue";
 import { useCounterStore } from "@/stores/counter";
 
 const counterStore = useCounterStore();
-// const count = computed(
-//   (): number => {
-//     return counterStore.counter;
-//   }
-// );
-const count = counterStore.counter;
+const count = computed(
+  (): number => {
+    return counterStore.counter;
+  }
+);
+// const count = counterStore.counter;
 
-// const doubleCount = computed(
-//   (): number => {
-//     return counterStore.doubleCount;
-//   }
-// );
-const doubleCount = counterStore.doubleCount;
+const doubleCount = computed(
+  (): number => {
+    return counterStore.doubleCount;
+  }
+);
+// const doubleCount = counterStore.doubleCount;
 
 const onIncrementClick = () => {
   counterStore.incrementCount();
