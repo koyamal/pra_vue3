9af27146e24bb16a22b9758f2b6a22d154d63469
koyamal <tanakamaruko1121n@gmail.com>
2023-05-10 16:09:06 +0900

    Add test of Screen in 8/2 Input

--- Vue3FrontDevTextBook/chap13/myUnitTest/src/__tests__/App.spec.ts
+++ Vue3FrontDevTextBook/chap13/myUnitTest/src/__tests__/App.spec.ts
@@ -14,5 +14,16 @@ describe(
         expect(actual).toBe(expected);
       }
     );
+    test(
+      "Test of Screen in 8/2 Input",
+      async () => {
+        const wrapper = mount(App);
+        await wrapper.get(`[data-testid="num1"]`).setValue(8);
+        await wrapper.get(`[data-testid="num2"]`).setValue(2);
+        const actual = wrapper.get(`[data-testid="ans"]`).text();
+        const expected = "4";
+        expect(actual).toBe(expected);
+      }
+    );
   }
 );
