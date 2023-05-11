069ff6bff9273b2dff43adca5fb4aac7d28db128
koyamal <tanakamaruko1121n@gmail.com>
2023-05-11 14:45:19 +0900

    Change App.vue to 8.5

--- Vue3FrontDevTextBook/chap13/myUnitTest/src/App.vue
+++ Vue3FrontDevTextBook/chap13/myUnitTest/src/App.vue
@@ -1,21 +1,39 @@
 <script setup lang="ts">
-import {ref} from "vue";
-
-const isVisible = ref(false);
-const onShowButtonClick = (): void => {
-  isVisible.value = true;
-}
+import {ref, computed} from "vue";
+import OneMember from "./components/OneMember.vue";
+const memberListInit = new Map<number, Member>();
+memberListInit.set(33456, {id: 33456, name: "Taro", email: "taro@exmaple.com", points: 35, note:"VIP"});
+memberListInit.set(47783, {id: 47783, name: "Jiro", email: "jiro@exmaple.com", points: 53});
+const memberList = ref(memberListInit);
+const totalPoints = computed(
+  (): number => {
+    let total = 0;
+    for(const member of memberList.value.values()){
+      total += member.points;
+    }
+    return total;
+  }
+);
+interface Member{
+  id: number;
+  name: string;
+  email: string;
+  points: number;
+  note?: string;
+};
 </script>
-
 <template>
-  <p>
-    <button
-      data-testid="showButton"
-      v-on:click="onShowButtonClick"
-    >
-      Show
-    </button>
-    hidden areas
-  </p>
-  <p v-if="isVisible" data-testid="invisible">Displayed</p>
+  <section>
+    <h1>Member's List</h1>
+    <p>Sum of Points: {{ totalPoints }}</p>
+    <OneMember
+      v-for="[id, member] in memberList"
+      v-bind:key="id"
+      v-bind:id="id"
+      v-bind:name="member.name"
+      v-bind:email="member.email"
+      v-model:points="member.points"
+      v-bind:note="member.note"
+    />
+  </section>
 </template>
