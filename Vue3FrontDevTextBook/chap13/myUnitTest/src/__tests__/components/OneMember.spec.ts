ddd1514ca4eb9a92ef600d74ff4fbccd2e9a5f40
koyamal <tanakamaruko1121n@gmail.com>
2023-05-11 15:06:42 +0900

    Create Test in Exist note value

---
+++ Vue3FrontDevTextBook/chap13/myUnitTest/src/__tests__/components/OneMember.spec.ts
@@ -0,0 +1,28 @@
+import {describe, test, expect} from "vitest";
+import {mount} from "@vue/test-utils";
+import OneMember from "@/components/OneMember.vue";
+
+describe(
+  "Test of OneMember",
+  () => {
+    test(
+      "Test in Exist note value",
+      () => {
+        const propsData = {
+          id: 22458,
+          name: "Saburo",
+          email: "sabu@example.com",
+          points: 200,
+          note: "Good",
+        };
+        const wrapper = mount(OneMember, {props: propsData});
+        const actualText = wrapper.text();
+        expect(actualText).toContain(String(propsData.id));
+        expect(actualText).toContain(propsData.name);
+        expect(actualText).toContain(propsData.email);
+        expect(actualText).toContain(String(propsData.points));
+        expect(actualText).toContain(propsData.note);
+      }
+    );
+  }
+);
