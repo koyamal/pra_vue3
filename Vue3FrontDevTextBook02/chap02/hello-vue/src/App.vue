61a2fbddbbac2b6d66fc4e7ab121d66cd877c776
koyamal <tanakamaruko1121n@gmail.com>
2023-06-24 17:59:30 +0900

    Delete

--- Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
+++ Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
@@ -1,29 +1,11 @@
 <script setup lang="ts">
 import {ref, computed} from "vue";
 
-const now = new Date();
 
-const nowStr = now.toLocaleTimeString();
-
-let timeStr = nowStr;
-
-const timeStrRef = ref(nowStr);
-
-function changeTime(): void {
-  const newTime = new Date();
-  const newTimeStr = newTime.toLocaleTimeString();
-
-  timeStr = newTimeStr;
-
-  timeStrRef.value = newTimeStr;
-}
-
-setInterval(changeTime, 1000);
 </script>
 
 <template>
-  <p>現在時刻：{{ timeStr }}</p>
-  <!-- <p>現在時刻(ref)：{{ timeStrRef }}</p> -->
+  <p>hello</p>
 </template>
 
 <style scoped>
