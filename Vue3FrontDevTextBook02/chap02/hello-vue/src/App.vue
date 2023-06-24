94a7c02c954d6407de0387cccfbe3165e3aaee7a
koyamal <tanakamaruko1121n@gmail.com>
2023-06-24 18:08:16 +0900

    Use reactive

--- Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
+++ Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
@@ -1,26 +1,25 @@
 <script setup lang="ts">
-import {ref, computed} from "vue";
+import {ref, computed, reactive} from "vue";
 
-const radiusInit = Math.round(Math.random() * 10);
-
-const PI = ref(3.14);
-
-const radius = ref(radiusInit);
+const data = reactive({
+  PI: 3.14,
+  radius: Math.round(Math.random() * 10),
+});
 
 const area = computed(
   (): number => {
-    return radius.value * radius.value * PI.value;
+    return data.radius * data.radius * data.PI;
   }
 );
 
 setInterval((): void => {
-  radius.value = Math.round(Math.random() * 10);
+  data.radius = Math.round(Math.random() * 10);
 }, 1000);
 </script>
 
 <template>
   <p>hello</p>
-  <p>半径{{ radius }}の円の面積を円周率{{ PI }}で計算すると、{{ area }}</p>
+  <p>半径{{ data.radius }}の円の面積を円周率{{ data.PI }}で計算すると、{{ area }}</p>
 </template>
 
 <style scoped>
