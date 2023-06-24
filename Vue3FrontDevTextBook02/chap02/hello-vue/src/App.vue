557898c6cda5e43f05ce4c6616aef13968ea002c
koyamal <tanakamaruko1121n@gmail.com>
2023-06-24 18:34:33 +0900

    Use v-bind

--- Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
+++ Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-const widthOrHeight = ref("height");
-const widthOrHeightValue = ref(300);
+const imgAttributes = ref({
+  src: "/images/logo.svg",
+  alt: "Vueのロゴ",
+  width: 75,
+  height: 75,
+});
 </script>
 
 <template>
-  <p><img src="./assets/logo.svg" alt="VueLogo" v-bind:[widthOrHeight]="widthOrHeightValue"></p>
+  <p><img v-bind="imgAttributes"></p>
+  <p><img v-bind="imgAttributes" title="ロゴです" alt="ロゴです"></p>
 </template>
 
 <style scoped>
