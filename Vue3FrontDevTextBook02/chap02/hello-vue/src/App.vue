496c2ba7b63144d89880101ce1aacc761dfc43ea
koyamal <tanakamaruko1121n@gmail.com>
2023-06-24 18:36:16 +0900

    Use width

--- Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
+++ Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
@@ -10,8 +10,9 @@ const imgAttributes = ref({
 </script>
 
 <template>
-  <p><img v-bind="imgAttributes"></p>
-  <p><img v-bind="imgAttributes" title="ロゴです" alt="ロゴです"></p>
+  <!-- <p><img v-bind="imgAttributes"></p>
+  <p><img v-bind="imgAttributes" title="ロゴです" alt="ロゴです"></p> -->
+  <p><img v-bind="imgAttributes" alt="ロゴです" width="600"></p>
 </template>
 
 <style scoped>
