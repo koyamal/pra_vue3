bd7302956834282d698834523c0468e944fb5b98
koyamal <tanakamaruko1121n@gmail.com>
2023-06-25 16:27:54 +0900

    Create onButtonMouseout

--- Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
+++ Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
@@ -3,6 +3,7 @@ import {ref} from "vue";
 
 const randValue = ref("まだです");
 const randValue2 = ref("まだです");
+const randValue3 = ref("まだです");
 const onButtonClick = (): void => {
   const rand = Math.round(Math.random() * 10);
   randValue.value = String(rand);
@@ -10,15 +11,19 @@ const onButtonClick = (): void => {
 const onButtonFocusout = (): void => {
   const rand = Math.round(Math.random() * 10);
   randValue2.value = String(rand);
-
+}
+const onButtonMouseout = (): void => {
+  const rand = Math.round(Math.random() * 10);
+  randValue3.value = String(rand);
 }
 </script>
 
 <template>
   <section>
-    <button v-on:click="onButtonClick" v-on:focusout="onButtonFocusout">クリック</button>
+    <button v-on:click="onButtonClick" v-on:focusout="onButtonFocusout" v-on:mouseout="onButtonMouseout">クリック</button>
     <p>onClick：{{randValue}}</p>
     <p>focusout：{{randValue2}}</p>
+    <p>mouseout：{{randValue3}}</p>
   </section>
 </template>
 
