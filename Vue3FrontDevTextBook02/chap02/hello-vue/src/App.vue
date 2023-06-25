cc7aa85bfb507e965f2a5244ee2fb08e4bddfc4a
koyamal <tanakamaruko1121n@gmail.com>
2023-06-25 16:26:05 +0900

    Create onButtonFocusout

--- Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
+++ Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
@@ -2,16 +2,23 @@
 import {ref} from "vue";
 
 const randValue = ref("まだです");
+const randValue2 = ref("まだです");
 const onButtonClick = (): void => {
   const rand = Math.round(Math.random() * 10);
   randValue.value = String(rand);
 };
+const onButtonFocusout = (): void => {
+  const rand = Math.round(Math.random() * 10);
+  randValue2.value = String(rand);
+
+}
 </script>
 
 <template>
   <section>
-    <button v-on:click="onButtonClick">クリック</button>
-    <p>クリックの結果：{{randValue}}</p>
+    <button v-on:click="onButtonClick" v-on:focusout="onButtonFocusout">クリック</button>
+    <p>onClick：{{randValue}}</p>
+    <p>focusout：{{randValue2}}</p>
   </section>
 </template>
 
