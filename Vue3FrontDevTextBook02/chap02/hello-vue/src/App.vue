f0f6b71ac472d1cadfcaf929f6392f5fbe50840c
koyamal <tanakamaruko1121n@gmail.com>
2023-06-25 09:58:20 +0900

    Create white or red

--- Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
+++ Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-const imgAttributes = ref({
-  src: "/images/logo.svg",
-  alt: "Vueのロゴ",
-  width: 75,
-  height: 75,
-});
+const whiteOrRed = ref("white");
+
+setInterval(() => {
+  if (whiteOrRed.value === "white") {
+    whiteOrRed.value = "red";
+  } else {
+    whiteOrRed.value = "white";
+  }
+}, 1000);
 </script>
 
 <template>
-  <!-- <p><img v-bind="imgAttributes"></p>
-  <p><img v-bind="imgAttributes" title="ロゴです" alt="ロゴです"></p> -->
-  <p><img v-bind="imgAttributes" alt="ロゴです" width="600"></p>
+  <p v-bind:style="{color: whiteOrRed}">White? or Red?</p>
 </template>
 
 <style scoped>
