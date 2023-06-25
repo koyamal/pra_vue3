bb75d33765683ffb9fb1fc0ef8b789ad761f1767
koyamal <tanakamaruko1121n@gmail.com>
2023-06-25 17:21:04 +0900

    Use select

--- Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
+++ Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
@@ -14,6 +14,15 @@ const memberTypeSelect = ref(1);
     <br>
     <p>選択されたラジオボタン：{{ memberType }}</p>
   </section>
+  <br>
+  <section>
+    <select v-model="memberTypeSelect">
+      <option value="1">通常会員</option>
+      <option value="2">特別会員</option>
+      <option value="3">優良会員</option>
+    </select>
+    <p>選択されたリスト：{{ memberTypeSelect }}</p>
+  </section>
 </template>
 
 <style scoped>
