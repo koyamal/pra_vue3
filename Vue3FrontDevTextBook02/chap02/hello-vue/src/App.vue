6e089c2553e307e835abe61d4a2f3d5bad7dc23d
koyamal <tanakamaruko1121n@gmail.com>
2023-06-25 10:07:49 +0900

    Create fontsize

--- Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
+++ Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {ref, computed} from "vue";
 
 const whiteOrRed = ref("white");
+
+const textSize = computed(
+  (): string => {
+    const size = Math.round(Math.random() * 25) + 10;
+    return `${size}pt`;
+  }
+);
 
 setInterval(() => {
   if (whiteOrRed.value === "white") {
@@ -14,6 +21,7 @@ setInterval(() => {
 
 <template>
   <p v-bind:style="{color: whiteOrRed}">White? or Red?</p>
+  <p v-bind:style="{color: whiteOrRed, fontSize:textSize}">White? or Red?</p>
 </template>
 
 <style scoped>
