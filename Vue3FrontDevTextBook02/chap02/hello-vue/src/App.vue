4bbfa3409cb66579f36627f3b35e7c04d9e1bc17
koyamal <tanakamaruko1121n@gmail.com>
2023-06-25 16:11:17 +0900

    Use v-on

--- Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
+++ Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
@@ -1,31 +1,18 @@
 <script setup lang="ts">
 import {ref, computed} from "vue";
 
-const whiteOrRed = ref("white");
-
-const textSizeRef = ref(`${Math.round(Math.random() * 25) + 10}pt`);
-
-const textSize = computed(
-  (): string => {
-    const size = Math.round(Math.random() * 25) + 10;
-    return `${size}pt`;
-  }
-);
-
-setInterval(() => {
-  textSizeRef.value = `${Math.round(Math.random() * 25) + 10}pt`;
-  if (whiteOrRed.value === "white") {
-    whiteOrRed.value = "red";
-  } else {
-    whiteOrRed.value = "white";
-  }
-}, 1000);
+const randValue = ref("まだです");
+const onButtonClick = (): void => {
+  const rand = Math.round(Math.random() * 10);
+  randValue.value = String(rand);
+};
 </script>
 
 <template>
-  <p v-bind:style="{color: whiteOrRed}">White? or Red?</p>
-  <p v-bind:style="{color: whiteOrRed, fontSize:textSize}">White? or Red?</p>
-  <p v-bind:style="{color: whiteOrRed, fontSize:textSizeRef}">White? or Red?</p>
+  <section>
+    <button v-on:click="onButtonClick">クリック</button>
+    <p>クリックの結果：{{randValue}}</p>
+  </section>
 </template>
 
 <style scoped>
