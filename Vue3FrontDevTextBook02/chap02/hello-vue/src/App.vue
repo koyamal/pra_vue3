4e3b6ec1e0dfd97ab8ac20385abb46010b64d274
koyamal <tanakamaruko1121n@gmail.com>
2023-06-25 16:49:49 +0900

    Add args and event

--- Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
+++ Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
@@ -2,15 +2,18 @@
 import {ref} from "vue";
 
 const pBgColor = ref("white");
-const onPClick = (bgColor: string): void => {
+const pMsg = ref("No");
+const onPClick = (bgColor: string, event: MouseEvent): void => {
   pBgColor.value = bgColor;
+  pMsg.value = event.timeStamp.toString();
 };
 </script>
 
 <template>
-  <p v-on:click="onPClick('red')" v-bind:style="{backgroundColor: pBgColor, color:'green'}">
+  <p v-on:click="onPClick('red', $event)" v-bind:style="{backgroundColor: pBgColor, color:'green'}">
     ここをクリックすると背景色が変わります。
   </p>
+  <p>{{ pMsg }}</p>
 </template>
 
 <style scoped>
