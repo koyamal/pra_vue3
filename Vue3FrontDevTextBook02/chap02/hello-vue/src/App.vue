2484421d2fa343856e91459620f93d7fe0a0953e
koyamal <tanakamaruko1121n@gmail.com>
2023-06-27 20:48:39 +0900

    Add p tag 5th

--- Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
+++ Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
@@ -35,6 +35,7 @@ const htmlStr = ref(`<a href="https://vuejs.org//">Vue.jsのTOPページ</a>`);
     <p>Hello, New Branch 02</p>
     <p>Hello, New Branch 03!</p>
     <p>Hello, New Branch 04</p>
+    <p>Hello, New Branch 05</p>
   </section>
 </template>
 
