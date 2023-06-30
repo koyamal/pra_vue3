1759774652968749ed3b7c78a5c7fc4643a9cb76
koyamal <tanakamaruko1121n@gmail.com>
2023-06-30 18:26:01 +0900

    Add -03

--- Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
+++ Vue3FrontDevTextBook02/chap02/hello-vue/src/App.vue
@@ -50,7 +50,7 @@ const onClickCountDown = () => {
     <p>Hello, New Branch 08</p>
     <p>Hello, New Branch 09!!!???</p>
     <p>Hello, New Branch 10-02-02</p>
-    <p>Hello, New Branch 11-03</p>
+    <p>Hello, New Branch 11-03-03</p>
   </section>
   <section>
     <p>Count: {{ counter }}</p>
