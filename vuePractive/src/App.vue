10ae5d63e10c440b75b62f549cc3a4c1d0d5b411
koyamal <tanakamaruko1121n@gmail.com>
2023-07-08 16:35:55 +0900

    Use vue3

---
+++ vuePractive/src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import { nextTick } from "vue";
+
+import MyButton from "./components/atom/Button.vue";
+export default {
+  components: {
+    MyButton
+  },
+
+  data() {
+    return {
+      count: 0,
+      ok: false,
+      seen: true,
+    }
+  },
+
+  methods: {
+    switchSeen() {
+      this.seen = !this.seen;
+    },
+    async increment() {
+      this.count++
+      await nextTick()
+      console.log("fin");
+    },
+  }
+}
+</script>
+
+<template>
+  <MyButton></MyButton>
+  <MyButton></MyButton>
+  <p>{{ ok? "Yes": "No" }}</p>
+  <div>
+    <p v-if="seen">Hello</p>
+    <button @click="switchSeen">Switch</button>
+  </div>
+  <div>
+    <button @click="increment">{{ count }}</button>
+  </div>
+</template>
