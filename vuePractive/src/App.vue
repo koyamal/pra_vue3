0b390488ed5f7ea97b891aacc602b62046b0e0e3
koyamal <tanakamaruko1121n@gmail.com>
2023-07-09 17:48:53 +0900

    Use vue3

--- vuePractive/src/App.vue
+++ vuePractive/src/App.vue
@@ -20,6 +20,24 @@ export default {
           'Vue 4 - The Mystery'
         ]
       },
+      isActive: false,
+      hasError: false,
+      items: [
+        {message: "Hello", time: "AM"},
+        {message: "Bye", time: "PM"},
+      ],
+      itemsObject: {
+        name: "Taro",
+        age: 21,
+        countory: "USA",
+      },
+      name: "taro",
+      text: "",
+      checked: false,
+      picked: "",
+      dynamicTrueValue: "OK",
+      dynamicFalseValue: "No",
+      toggle: "",
     }
   },
 
@@ -54,6 +72,22 @@ export default {
     nowInMethods() {
       return Date.now();
     },
+    greet(event) {
+      console.log(`Hello ${this.name}!`)
+      if (event) {
+        console.log(event.target.tagName)
+      }
+    },
+    sayText(message) {
+      console.log(message);
+    },
+    warn(message, event) {
+      if (event) {
+        console.log(event.clientX);
+        event.preventDefault();
+      }
+      alert(message);
+    },
   }
 }
 </script>
@@ -76,4 +110,52 @@ export default {
   </div>
   <div>Computed: {{ now }}</div>
   <div>Method: {{ nowInMethods() }}</div>
+  <div class="static" :class="{active: isActive, textDanger: hasError}">Hello</div>
+  <div>
+    <li v-for="item in items">
+      {{ item.time }} : {{ item.message }}
+    </li>
+  </div>
+  <div>
+    <li v-for="({time, message}, index) in items">
+      {{ index }} {{ time }} : {{ message }}
+    </li>
+  </div>
+  <div>
+    <li v-for="(value, key) in itemsObject" :key="value">
+      {{ key }} : {{ value }}
+    </li>
+  </div>
+  <div>
+    <button @click="greet">Greet</button>
+    <button @click="sayText('Hello, Bob')">Message</button>
+    <button @click="warn('Form cannot be submitted yet.', $event)">Submit</button>
+    <button @click="(event) => warn('Form cannot be submitted yet.', event)">Arrow</button>
+  </div>
+  <div>
+    <p>{{ text }}</p>
+    <input :value="text" @input="event => text = event.target.value">
+    <input v-model="text">
+  </div>
+  <div>
+    <input type="checkbox" id="checkbox" v-model="checked" />
+    <label for="checkbox">{{ checked }}</label>
+  </div>
+  <div>
+    <div>Picked: {{ picked }}</div>
+
+    <input type="radio" id="one" value="One" v-model="picked" />
+    <label for="one">One</label>
+
+    <input type="radio" id="two" value="Two" v-model="picked" />
+    <label for="two">Two</label>
+  </div>
+  <div>
+    <input
+      type="checkbox"
+      v-model="toggle"
+      :true-value="dynamicTrueValue"
+      :false-value="dynamicFalseValue" />
+      <p>{{ toggle }}</p>
+  </div>
 </template>
