c5bea87d8b8d5abcfe62c60f9326e2dd2d8dc63d
koyamal <tanakamaruko1121n@gmail.com>
2023-07-12 20:19:10 +0900

    AlertBox コンポーネントを作成した。

---
+++ vuePractive/src/components/atom/AlertBox.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="alert-box">
+    <strong>This is an Error for Demo Purposes</strong>
+      <slot />
+  </div>
+</template>
+  
+<style>
+.alert-box {
+  color: red;
+  font-size: 32px;
+}
+</style>
