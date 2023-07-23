779a1509a5fba42485c569dd2dd46722eb26106e
koyamal <tanakamaruko1121n@gmail.com>
2023-07-23 18:29:56 +0900

    Create E

---
+++ vuePractive/src/components/atom/E.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      count: "Text"
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p>{{ count }}</p>
+    <button @click="() => count += 'T'"></button>
+  </div>
+</template>
