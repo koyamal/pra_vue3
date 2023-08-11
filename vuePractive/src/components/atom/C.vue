4d9268cb919b9589da764502c1c70f3becb8b023
koyamal <tanakamaruko1121n@gmail.com>
2023-08-11 18:58:20 +0900

    Add numberList

--- vuePractive/src/components/atom/C.vue
+++ vuePractive/src/components/atom/C.vue
@@ -1,3 +1,14 @@
+<script>
+  export default {
+  data() {
+    return {
+      numberList: [
+        1, 3, 3, 4, 5
+      ]
+    }
+  },
+  }
+</script>
 <template>
   <div>This is C</div>
 </template>
