20308af6fbb1156f163ee4d072ab841ffe8d0f57
koyamal <tanakamaruko1121n@gmail.com>
2023-08-11 20:36:27 +0900

    Create editSpreadNumberList

--- vuePractive/src/components/atom/C.vue
+++ vuePractive/src/components/atom/C.vue
@@ -19,6 +19,12 @@
       copyNumberList[1] = 2;
       console.log(copyNumberList);
       console.log(this.numberList);
+    },
+    editSpreadNumberList() {
+      const spreadNumberList = [...this.numberList];
+      spreadNumberList[1] = 2;
+      console.log(spreadNumberList);
+      console.log(this.numberList);
     }
   }
   }
