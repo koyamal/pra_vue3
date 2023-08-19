f49efe622e1ac34f2a105f13f08dce1cdb4949ba
koyamal <tanakamaruko1121n@gmail.com>
2023-08-19 17:57:39 +0900

    Create funcA

--- vuePractive/src/components/atom/F.vue
+++ vuePractive/src/components/atom/F.vue
@@ -4,8 +4,17 @@
       return {}
     },
     methods: {
+      funcA() {
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            console.log('hello');
+            resolve('end');
+          }, 500);
+        });
+      }
     }
   }
 </script>
 <template>
+  <button @click="funcA">funcA</button>
 </template>
