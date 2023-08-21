788f66a1b84472d201e26ec5dccdf1542ae438b2
koyamal <tanakamaruko1121n@gmail.com>
2023-08-21 21:08:30 +0900

    Add funcD to promise.all

--- vuePractive/src/components/atom/F.vue
+++ vuePractive/src/components/atom/F.vue
@@ -38,7 +38,7 @@
       },
       async execFuncAB() {
         const a = await Promise.all([
-          this.funcA(), this.funcB(), this.funcC()
+          this.funcA(), this.funcB(), this.funcC(), this.funcD()
         ]);
       }
     }
