5c696eb7f1b354e4572387e76e8e25ba1608ece8
koyamal <tanakamaruko1121n@gmail.com>
2023-09-05 21:08:49 +0900

    Create G.vue

---
+++ vuePractive/src/components/atom/G.vue
@@ -0,0 +1,19 @@
+<script>
+  import asyncFunctions from './functions';
+  export default {
+    data() {
+      return {
+      }
+    },
+    computed: {
+    },
+    methods: {
+      execFunctions() {
+        asyncFunctions(100, 'hello', 'world');
+      }
+  },
+}
+</script>
+<template>
+  <button @click="execFunctions">Do</button>
+</template>
