464584e623cd4d623bbf58d4a8bfe161021e3627
koyamal <tanakamaruko1121n@gmail.com>
2023-09-08 21:29:54 +0900

    Edit output

--- vuePractive/src/components/atom/G.vue
+++ vuePractive/src/components/atom/G.vue
@@ -34,7 +34,7 @@
       async execPromiseAll3() {
         const a = await Promise.all([
           this.execFunctions(1000 ,'Hello3-1', 'World3-1'),
-          this.execFunctions(2000 ,'Hello3-2', 'World2-2'),
+          this.execFunctions(2000 ,'Hello3-2', 'World3-2'),
           this.execFunctions(3000 ,'Hello3-3', 'World3-3'),
         ]);
         return a;
