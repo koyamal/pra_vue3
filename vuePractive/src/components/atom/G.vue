044d6059177c46adc00cf3efee87852c8f86dbd0
koyamal <tanakamaruko1121n@gmail.com>
2023-10-21 21:04:34 +0900

    Add forEach

--- vuePractive/src/components/atom/G.vue
+++ vuePractive/src/components/atom/G.vue
@@ -54,6 +54,11 @@
           let bumbBox = [];
           if(i % 2 === 1) {
             bumbBox = [1, 3, 5];
+            let ans = 0;
+            bumbBox.forEach((bumb) => {
+              ans += bumb;
+            });
+            console.log(ans);
           } else {
             bumbBox = [2, 4, 6];
           }
