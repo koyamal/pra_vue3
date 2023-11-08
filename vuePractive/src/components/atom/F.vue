2570c374828608ca5135274cd516c512708cfb1f
koyamal <tanakamaruko1121n@gmail.com>
2023-11-08 21:51:38 +0900

    Edit

--- vuePractive/src/components/atom/F.vue
+++ vuePractive/src/components/atom/F.vue
@@ -18,7 +18,7 @@
         if (this.adminFlag) return true;
         dataInt+=10;
         dataInt-= 10;
-        return false;
+        return dataInt === 1;
       },
       showMessage() {
         return this.isWriter? 'Writer': 'No writer';
