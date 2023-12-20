40ddd5793888fa11c900f5fe06e5315ad44e525f
koyamal <tanakamaruko1121n@gmail.com>
2023-12-20 18:51:30 +0900

    Edit

--- vuePractive/src/node/regularExpression.js
+++ vuePractive/src/node/regularExpression.js
@@ -1,6 +1,7 @@
 const checkText = function(value) {
-  return /^(?=.*[/s]).*$/.test(value);
+  return /^(?=.*[\s]).*$/.test(value);
 }
 
 
 console.log(checkText("helle"));
+console.log(checkText("hel le"));
