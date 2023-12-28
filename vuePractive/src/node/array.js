6640b63dad85d66a23f72697afdc8baf0b0b5957
koyamal <tanakamaruko1121n@gmail.com>
2023-12-28 17:50:31 +0900

    Edit

--- vuePractive/src/node/array.js
+++ vuePractive/src/node/array.js
@@ -1,5 +1,9 @@
-const arr1 = new Array(element0, element1, elementN);
-const arr2 = Array(element0, element1, elementN);
-const arr3 = [element0, element1, elementN];
+const element0 = {name: 'Tom', age: 18};
+const element1 = {name: 'Bob', age: 23};
+const element2 = {name: 'Jhon', age: 22};
+
+const arr1 = new Array(element0, element1, element2);
+const arr2 = Array(element0, element1, element2);
+const arr3 = [element0, element1, element2];
 
 console.log(arr1);
