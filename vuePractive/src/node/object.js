732413b6f79371f19c6899002d531646252150e1
koyamal <tanakamaruko1121n@gmail.com>
2024-01-01 23:50:18 +0900

    Edit

--- vuePractive/src/node/object.js
+++ vuePractive/src/node/object.js
@@ -6,3 +6,18 @@ myCar.year = 1969;
 console.log(myCar);
 console.log(myCar.color);
 console.log(myCar["make"]);
+
+
+var myObj = new Object(),
+  str = "myString",
+  rand = Math.random(),
+  obj = new Object();
+
+myObj.type = "ドット表記";
+myObj["date created"] = "空白入りの文字列";
+myObj[str] = "文字列の値";
+myObj[rand] = "乱数";
+myObj[obj] = "オブジェクト";
+myObj[""] = "空文字列も可能";
+
+console.log(myObj);
