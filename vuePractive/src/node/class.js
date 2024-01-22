987417fee20d017edde55879d8cc8b753f174e67
koyamal <tanakamaruko1121n@gmail.com>
2024-01-22 20:35:02 +0900

    Edit

--- vuePractive/src/node/class.js
+++ vuePractive/src/node/class.js
@@ -93,3 +93,7 @@ console.log(Color.isValid(2, 3, 5));
 const colorA = new Color(2, 3, 5);
 // console.log(colorA.isValid(2, 3, 5)); // error
 // console.log(colorA.isValid()); // error
+
+class ColorWithAlpha extends Color {
+
+}
