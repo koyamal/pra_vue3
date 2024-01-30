473b384c0d45238eb66e5832088a1dc87282f55e
koyamal <tanakamaruko1121n@gmail.com>
2024-01-30 21:13:47 +0900

    Edit

--- vuePractive/src/node/promise.js
+++ vuePractive/src/node/promise.js
@@ -1,9 +1,9 @@
 const promiseFunc = new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log("hello");
-    // resolve();
-    reject("!!");
-  }, 1000);
+    resolve("??");
+    // reject("!!");
+  }, 300);
 });
 
 promiseFunc.then((val) => {console.log("done", val)}, (val) => {console.log("reject", val)});
