ae82cb3f96d3e3626c1a483dc06f2efec7aa9891
koyamal <tanakamaruko1121n@gmail.com>
2024-01-31 20:16:05 +0900

    Edit

--- vuePractive/src/node/promise.js
+++ vuePractive/src/node/promise.js
@@ -6,12 +6,10 @@ const promiseFunc = new Promise((resolve, reject) => {
   }, 300);
 });
 
-console.log(promiseFunc)
-
-// promiseFunc.then(
-//   (val) => {console.log("done", val)},
-//   (val) => {console.log("reject", val)}
-// );
+const b = promiseFunc.then(
+  (val) => {console.log("done", val)},
+  (val) => {console.log("reject", val)}
+);
 
 // const a = promiseFunc;
 // const b = a.then();
