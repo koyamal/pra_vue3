3cd9f0d340cbcb11dea6b23c4d90edb0434fa391
koyamal <tanakamaruko1121n@gmail.com>
2024-03-27 21:06:26 +0900

    Edit

--- vuePractive/src/node/date.js
+++ vuePractive/src/node/date.js
@@ -95,7 +95,8 @@ const wait = (sec, rejectFlag = false) => {
   return new Promise((resolve, reject) => {
     if (rejectFlag) reject("reject called");
     setTimeout(() => {resolve("resolve called")}, sec * 100);
-  }).catch(() => {
+  }).catch((e) => {
+    console.log(e);
     throw new Error('Error is occured');
   });
 }
