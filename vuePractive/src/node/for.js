b7c932015f3627b13b697357fa9ec6df01ea4872
koyamal <tanakamaruko1121n@gmail.com>
2024-04-08 20:21:07 +0900

    Edit

---
+++ vuePractive/src/node/for.js
@@ -0,0 +1,9 @@
+const wait = (sec, rejectFlag = false) => {
+  return new Promise((resolve, reject) => {
+    if (rejectFlag) reject("reject called");
+    setTimeout(() => {resolve("resolve called")}, sec * 100);
+  }).catch((e) => {
+    console.log(e);
+    throw new Error('Error is occured');
+  });
+}
